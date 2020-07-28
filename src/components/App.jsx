d880d12d59f377c77cc4a9cb62de453a8d5e3a42
LivUyseang <livuyseang@gmail.com>
2020-07-28 09:35:33 +0700

    start

---
+++ src/components/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="twilight-head">
+        <h1>Twilight</h1>
+          <div class="twilight-header-button">
+            <button type="button" className="twilight-search">SEARCH</button>
+            <button type="button" className="twilight-signin">SIGN IN</button>
+          </div>
+      </div>
+
+      <div className="intro-image">
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
