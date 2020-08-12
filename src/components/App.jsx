a558b1be2995d3a28a1bf33002a14ff80f1d01e0
LivUyseang <livuyseang@gmail.com>
2020-08-12 08:57:25 +0700

    Before convert to NodeJS

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -24,11 +24,55 @@ function App() {
         <div className="best-line"></div>
       </div>
 
+      <div className="product-card">
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+      </div>
+
+      <div className="new-arrival">
+        <h2>NEW ARRIVAL</h2>
+        <div className="best-line-2"></div>
+      </div>
+
+      <div className="product-card">
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+      </div>
+
       <div>
+        <h2>PROMOTION</h2>
+        <div className="best-line-3"></div>
+      </div>
+
+      <div className="product-card">
         <Card/>
         <Card/>
         <Card/>
         <Card/>
+      </div>
+
+      <div className="footer">
+        <div className="first">
+          <p>Twilight</p>
+          <p>About Us</p>
+          <p>Facebook</p>
+          <p>No. 1, St. 2, Phnom Penh</p>
+        </div>
+        <div className="second">
+          <p>Contact</p>
+          <p>Twitter</p>
+          <p>+855 12 345 678</p>
+        </div>
+        <div className="third">
+          <p>2020 Twilight. All rights reserved</p>
+          <p>Terms & Conditions</p>
+          <p>Instagram</p>
+          <p>twilightskincare@gmail.com</p>
+        </div>
       </div>
 
     </div>
--- src/components/product-card.jsx
+++ src/components/product-card.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Card() {
   return (
-    <div className="product-card">
+    <div className="card">
     </div>
   );
 }
